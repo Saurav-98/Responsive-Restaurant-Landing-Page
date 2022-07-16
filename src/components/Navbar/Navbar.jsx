e4577f05bcda2d6b0f19b8a043ae4b06d1c03fa2
Saurav Kumar Verma <sauravkumarverma22@gmail.com>
2022-07-16 12:13:49 +0530

    Navbar + Responsive Navbar completed

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,11 +1,86 @@
-import React from 'react';
+import React from "react";
 
-import './Navbar.css';
+import "./Navbar.css";
+import { GiHamburgerMenu, GiKnifeFork } from "react-icons/gi";
 
-const Navbar = () => (
-  <div>
-    Navbar
-  </div>
-);
+import { images } from "../../constants/index";
+
+const Navbar = () => {
+  const [toggle, setToggle] = React.useState(false);
+
+  return (
+    <nav className="app-navbar">
+      <div className="app__navbar-logo">
+        <a href="#home">
+          <img src={images.gericht} alt="gericht-logo" />
+        </a>
+      </div>
+      <ul className="app__navbar-links">
+        <li className="p__opensans">
+          <a href="#home">Home</a>
+        </li>
+        <li className="p__opensans">
+          <a href="#pages">Pages</a>
+        </li>
+        <li className="p__opensans">
+          <a href="#contactus">Contact Us</a>
+        </li>
+        <li className="p__opensans">
+          <a href="#blog">Blog</a>
+        </li>
+        <li className="p__opensans">
+          <a href="#landing">Landing</a>
+        </li>
+      </ul>
+      <div className="app__navbar-login">
+        <a href="#home" className="p__opensans">
+          Login / Register
+        </a>
+        <div />
+        <a href="/" className="p__opensans">
+          Book Table
+        </a>
+      </div>
+
+      <div className="app__navbar-smallscreen_links">
+        <GiHamburgerMenu
+          color="var(--color-golden)"
+          fontSize={27}
+          onClick={() => {
+            setToggle(true);
+          }}
+        />
+        {toggle && (
+          <div className="app__navbar-smallscreen_overlay flex__center slide-bottom">
+            <GiKnifeFork
+              className="overlay__close"
+              fontSize={27}
+              onClick={() => {
+                setToggle(false);
+              }}
+            />
+            <ul className="app__navbar-smallscreen_links">
+              <li className="p__opensans">
+                <a href="#home">Home</a>
+              </li>
+              <li className="p__opensans">
+                <a href="#pages">Pages</a>
+              </li>
+              <li className="p__opensans">
+                <a href="#contactus">Contact Us</a>
+              </li>
+              <li className="p__opensans">
+                <a href="#blog">Blog</a>
+              </li>
+              <li className="p__opensans">
+                <a href="#landing">Landing</a>
+              </li>
+            </ul>
+          </div>
+        )}
+      </div>
+    </nav>
+  );
+};
 
 export default Navbar;
